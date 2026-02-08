a12d2f37fee5d3b26956405247c283f65b19c1e5
Pratco <135444337+Pratco@users.noreply.github.com>
2026-02-07 21:00:38 -0400

    Delete desktop directory

--- desktop/src-tauri/src/tray_notify.rs
+++
@@ -1,9 +0,0 @@
-use tauri::{AppHandle};
-
-#[tauri::command]
-pub fn tray_notify(app: AppHandle, title: String, body: String) {
-    let _ = tauri::api::notification::Notification::new(&app.config().tauri.bundle.identifier)
-        .title(&title)
-        .body(&body)
-        .show();
-}
